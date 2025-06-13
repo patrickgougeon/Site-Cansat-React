a5ac6a0fbefe9f73c39d8720f66ff4a0aee1b5fe
joao245br1 <66421174+joao245br1@users.noreply.github.com>
2025-06-13 06:13:26 -0300

    Atualizar o inscricao.jsx

--- src/pages/inscricao.jsx
+++ src/pages/inscricao.jsx
@@ -6,11 +6,17 @@ import Formulario from "../components/Formulario";
     return (
       <>
         <Navbar />
+
+      {/* Página de Inscrição */}
+
         <main id="inscricao">
           <div id="apresentacao-insc">
             <h1>Página de Inscrição</h1>
             <p>Preencha o formulário abaixo para se inscrever e gerar seu comprovante em PDF.</p>
           </div>
+
+          {/* Formulário de inscrição */}
+
           <Formulario />
         </main>
         <Footer />
